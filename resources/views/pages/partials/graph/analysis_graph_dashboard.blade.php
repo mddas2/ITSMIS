ff3ba47f2e6c6aec01f3921d0aea8e5f72abefe0
mddas <mddas>
2023-01-09 22:59:23 +0545

    filter in grph

--- resources/views/pages/partials/graph/analysis_graph_dashboard.blade.php
+++ resources/views/pages/partials/graph/analysis_graph_dashboard.blade.php
@@ -1,3 +1,97 @@
+<div class="card-body">
+            <form>
+                <div class="form-group row">
+                    <div class="col-lg-3">
+                        <label>From Date:</label>
+                        <input name="from_date" class="form-control form-control-solid nepdatepicker" data-single="true"
+                               required
+                               value="{{$from_date}}">
+                    </div>
+                    <div class="col-lg-3">
+                        <label>To Date:</label>
+                        <input name="to_date" class="form-control form-control-solid nepdatepicker" data-single="true"
+                               required
+                               value="{{$to_date}}">
+                    </div>
+                    <div class="col-lg-3">
+                        <label>Items:</label>
+                        <?php
+                        $itemList = ["" => "Select Items"];
+                        $itemList = $itemList + $items;
+                        ?>
+                        {{Form::select('item_id',$itemList,$item_id,['class' => 'form-control'])}}
+                    </div>
+                    <div class="col-lg-2" style="margin-top: 24px;">
+                        <button type="submit" class="btn btn-secondary">Filter</button>
+                    </div>
+                </div>
+            </form>
+            @if($data->count()>0)
+                <table class="table table-bordered table-hover table-checkable mt-10 table-striped" id="kt_datatable">
+                    <thead>
+                    <tr>
+                        <th>SN</th>
+                        <th>Date</th>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Quantity Unit</th>
+                        <th>Stock Quantity</th>
+                        <th>Sales Quantity</th>
+
+                    </tr>
+
+                    </thead>
+                    <tbody>
+                    <?php  $key = 0; ?>
+                    @foreach($data as $row)
+
+                        @if($row->date == $row->salesDate)
+
+                        <tr>
+                            <td>{{$key+1}}</td>
+                            <td> {{$row->date}} </td>
+                            <td>
+                                @foreach($items as $a => $item)
+                                    @if($row->item_id == $a)
+                                        {{$item}}
+                                    @endif
+                                @endforeach
+                            </td>
+                            <td>
+                                {{$row->quantity}}
+                            </td>
+                            <td>
+                                @foreach($units as $b => $unit)
+                                    @if($row->quantity_unit == $b)
+                                        {{$unit}}
+                                    @endif
+                                @endforeach
+                            </td>
+
+
+                                <td>
+                                    {{$row->stock_quantity}}
+                                </td>
+
+                                <td>
+                                    {{$row->sales_quantity}}
+                                </td>
+
+                        </tr>
+                        @php $key++; @endphp
+                        @endif
+
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+
+
+        </div>
+
+
+
+
 <link href="/chart/dist/css/style.min.css" rel="stylesheet">
 
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
@@ -20,7 +114,7 @@
             
                 <div class="col-6">
                 <div class="card-body">
-                                <h4 class="card-title">Provience data Table</h4>
+                                <h4 class="card-title">Provience data Table of Wheat</h4>
                                 <!-- <h6 class="card-subtitle">Add<code>.table-striped</code>for borders on all sides of the table and cells.</h6> -->
                                 <div class="table-responsive">
                                     <table class="table table-striped">
