c968cbcbc0a0240dc13b0c6546911759a14e9ff6
mddas <mddas>
2023-01-29 15:43:18 +0545

    some

--- app/Http/Controllers/ForeCastController.php
+++ app/Http/Controllers/ForeCastController.php
@@ -616,26 +616,19 @@ class ForeCastController extends Controller
     }
     public function AjaxGetYearlyData(Request $request){
 
-        $year = $request['year'];
+        $current_year = $request['year'];
 
         $item_id = $request['item_id'];
         $item_obj = Consumption::all()->where("item_id",$item_id)->sum("quantity"); 
 
-        $year_obj = Consumption::where("item_id",$item_id)->whereYear('date', '=', $year);
 
         // $monthly_data = [];
       
-
-        // for($month = 1; $month<=12; $month++){
-        //     // $variable = "month_".$month;
-        //     $monthly_data[$month] = Consumption::where("item_id",$item_id)->whereYear('date', '=', $year)->whereMonth('date','=',$month)->get()->sum("quantity");
-        // }
         $data = [];
-        $data[0] = array("y"=>2006,"a"=>100,"b"=>90,"c"=>60);
-        $data[1] = array("y"=>2006,"a"=>100,"b"=>90,"c"=>60);
-        $data[2] = array("y"=>2006,"a"=>100,"b"=>90,"c"=>60);
-        $data[3] = array("y"=>2006,"a"=>100,"b"=>90,"c"=>60);
-        $data[4] = array("y"=>2006,"a"=>100,"b"=>90,"c"=>60);
+        for($year = 0; $year<=6; $year++){
+            $year_sum = Consumption::where("item_id",$item_id)->whereYear('date', '=', $current_year-$year)->get()->sum("quantity");
+            $data[$year] = array("y"=>$current_year-$year,"a"=>$year_sum,"b"=>90,"c"=>60);
+        }
         return $data;
     }
     
